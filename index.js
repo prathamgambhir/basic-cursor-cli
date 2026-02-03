0f8dc50b01b40bfa61aaae42dcb37bd5f36d18f5
Pratham Gambhir <prathamgambhir0307@gmail.com>
2026-02-03 14:10:00 +0530

    add functions

--- index.js
+++ index.js
@@ -1,4 +1,75 @@
+import { GoogleGenAI } from "@google/genai";
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
+import readlineSync from "readline-sync";
 
+const ai = new GoogleGenAI({});
 
+async function read_file({ file_path }) {
+  const content = await fs.readFileSync(file_path, "utf-8");
+  console.log(`writing in : ${file_path}`);
+  return { content };
+}
 
+async function write_file({ file_path, content }) {
+  await fs.writeFileSync(file_path, content, "utf-8");
+  console.log(`writing in : ${file_path}`);
+  return { success: true };
+}
+
+async function make_folder({ folder_path }) {
+  fs.mkdirSync(folder_path, { recursive: true });
+  console.log(`made folder : ${folder_path}`);
+  return { success: true };
+}
+
+const readFileDecleratioin = {
+  name: "read_file",
+  description: "Read a file.",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {
+      file_path: {
+        type: Type.STRING,
+        description: "The location of the file to read.",
+      },
+    },
+    required: ["file_path"],
+  },
+};
+
+const writeFileDecleratioin = {
+  name: "write_file",
+  description: "write content in the file",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {
+      file_path: {
+        type: Type.STRING,
+        description: "The location of the file to write in or create.",
+      },
+      content: {
+        type: Type.STRING,
+        description: "The content to write in the file.",
+      }
+    },
+    required: ["file_path", "content"],
+  },
+};
+
+const makeFolderDecleratioin = {
+  name: "make_folder",
+  description: "create a folder/directory.",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {
+      file_path: {
+        type: Type.STRING,
+        description: "The location of the folder/directory to create.",
+      },
+    },
+    required: ["file_path"],
+  },
+};
 
